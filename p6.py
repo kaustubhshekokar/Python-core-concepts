d30b1ec7d7a19da0f5d94bc2cf3234d3b0edc046
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-01 11:04:59 +0530

    day 2

---
+++ p6.py
@@ -0,0 +1,14 @@
+print("Hello"+"World")
+print("Hello"+" "+"World")
+print("String Manupulation Exercise\nString Concatenation is done with "+"+ Sign")
+print('e.g. print"Hello"+"Jenny")')
+print("New lines can be created with a backlash and n")      
+
+
+
+
+
+
+
+
+
