73bba408ca707a333fa166841596ffe2e3058768
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-03 22:13:22 +0530

    Day 4

---
+++ p12.py
@@ -0,0 +1,26 @@
+#lenght=len("Kaustubh Shekokar")
+#print(10+1)
+#print(lenght)
+
+#print(int("10")+int("10"))
+#print("your name has "+str(lenght)+" letters")
+#print("10"+"10")
+#name="Kaustubh" 
+
+#print(str(name))
+
+a=input("Enter the first no.")
+b=input("Enter the second no.")
+sum=int(a)+int(b)
+name="sum of the numbers is "
+print(sum)
+
+
+
+
+
+
+
+
+
+
