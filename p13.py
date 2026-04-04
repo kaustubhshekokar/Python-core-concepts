3f463487cded09d21bb19f757d414da72debadab
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-04 19:16:30 +0530

    Dat 5

---
+++ p13.py
@@ -0,0 +1,6 @@
+a=input("enter the number")
+ 
+sum1=(a[0])
+sum2=(a[1])
+sum=int(sum1)+int(sum2)
+print(sum)
---
+++ p14.py
@@ -0,0 +1,11 @@
+a=2
+b=3
+c=a+b
+c//=a
+print(c)
+ 
+p=5
+print(p==5)
+print(p>=5)
+print(p<5)
+print(p!=5)
