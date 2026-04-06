79953689f89f06c811fb5ecb34d2d4e5fb166502
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-06 18:11:20 +0530

    day 6

---
+++ p18.py
@@ -0,0 +1,7 @@
+a=5
+b=6
+print(a is b)
+print(a is not b)
+print(id(a)==id(b))
+print(id(a))
+print(id(b))
---
+++ p19.py
@@ -0,0 +1,8 @@
+#str='Kaustubh'
+
+#print('K' in str)
+#print('l' not in str)
+
+l1=[2,3,4,5,0]
+
+print(0 not in l1)
