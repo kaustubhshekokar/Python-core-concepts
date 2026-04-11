adb121132f2f28963c941ae411ab89adc7f5c194
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-11 22:28:18 +0530

    Day10

---
+++ p24_if.py
@@ -0,0 +1,12 @@
+"""height=int(input('Enter the height:'))
+
+if(height>3):
+    print("you are elegible")
+print("out of block")"""   
+
+height=int(input('Enter the height:'))
+
+if(height>3):
+    print("you are elegible")
+else:
+    print("not eligible")
---
+++ p25_exercise.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the no.:"))
+
+if(num%2==0):
+    print("the number is even")
+
+else:
+    print("the number is odd")
+        
+  
