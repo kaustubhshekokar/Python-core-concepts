37969adf1fa1d2218e00dc78787626992d02f50f
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-04-15 11:37:43 +0530

    Day12

---
+++ p27_exercise_BMIrange.py
@@ -0,0 +1,17 @@
+weight=input('what is weight?')
+height=input('what is height?')
+
+BMI=int(weight)//float(height) **2
+print('BMI is',BMI)
+if BMI<16:
+    print("Under...")
+elif 16<BMI<20:
+    print("Under..")    
+elif 20<BMI<24:
+    print("Under..")
+elif BMI>=40:
+    print("obese")
+else:
+    print("Mar !!!!!!!")
+
+print("Boye boye")                    
