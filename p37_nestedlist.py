ecedc6a380c09a7e9cd6f8748cce44f8a1ab24ff
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-05-06 18:08:26 +0530

    day 20

---
+++ p37_nestedlist.py
@@ -0,0 +1,5 @@
+list1=[55,6,22,6,[3,6,-1],-3]
+len(list1)
+print(list1[4])
+print(list1[4][1])
+#somewhat playing in lens and list brackets...
---
+++ p38_exer.py
@@ -0,0 +1,18 @@
+list1=[[1,1,1],[1,1,1],[1,1,1]]
+x=int(input("enter the row no."))
+y=int(input("enter the column no."))
+
+"""list1[x-1][y-1]="x"
+#print(a)
+print(list1)"""
+
+row1=[1,1,1]
+row2=[1,1,1]
+row3=[1,1,1]
+row=[row1,row2,row3]
+
+row[x-1][y-1]="x"
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
