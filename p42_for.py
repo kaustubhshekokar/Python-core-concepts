3d0fa7b4f57aa98e2db58906236966f5e69c1681
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-05-10 00:27:12 +0530

    day 22

---
+++ p42_for.py
@@ -0,0 +1,25 @@
+'''name=['Kaustubh','ram','sam']
+for i in name:
+    print(i)
+    if i=='Kaustubh':
+        print("you are intelligent")'''
+list1=[2,3,4,-2]
+squares=[]
+for i in list1:
+    square=i**2
+    squares.append(square)
+    print(square)
+print("the list of squares",squares)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p43_for_else.py
@@ -0,0 +1,27 @@
+'''tuple1=(2,3,4,5,6)
+for i in tuple1:
+    print(i)
+    if i==5:
+        break
+else:
+    print("Loop successfully completed")    
+print("change the if loop inside for to see output")
+'''
+tuple2=(2,3,44,53,-1)
+for i in tuple2:
+    if i%6==0:
+        print(i)
+        break
+    #else:
+      #  print("no no. is divisible by 6")
+else:
+     print("no no. is divisible by 6")
+
+
+
+
+
+
+
+
+
