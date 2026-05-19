f8fb23fea327b10cb613d201bcddcd78c74c0787
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-05-19 22:41:26 +0530

    day 27

---
+++ p59_exer.py
@@ -0,0 +1,65 @@
+'''def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():   
+    turn_left()
+    move()
+    while wall_on_right():
+        move()
+    else:   
+        turn_right()
+        move()
+        turn_right()
+    while front_is_clear():
+        move()
+    else:
+        turn_left()
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+ 
+'''
+     
+#by jenny
+"""
+def turn_right():
+turn_left()
+turn_left()
+turn_left()
+def jump():
+turn_left()
+while wall_on_right():
+move ()
+turn_right()
+move ()
+turn_right()
+while front_is_clear():
+move ()
+turn_left()
+
+while not at_goal():
+if wall_in_front():
+jump ()
+else:
+move ()"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p60_argument.py
@@ -0,0 +1,19 @@
+def sum(a,b):
+    add=a+b
+    print("the sum is",add)
+    print(f"the sum is {add}")
+
+sum(3,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
