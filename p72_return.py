81fd038d0252c309208e835765a64b368a24ddbb
Kaustubh Shekokar <kaustubhshekokar@gmail.com>
2026-05-27 17:52:21 +0530

    day 33

---
+++ p72_return.py
@@ -0,0 +1,27 @@
+
+def add(a,b):
+    c=a+b
+    return c
+
+
+
+c=add(5,4)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p73_return.py
@@ -0,0 +1,34 @@
+
+"""def add(a,b):
+    c=a+b
+    return c
+
+print(add(5,4))
+result=add(5,4)
+print(result)
+
+
+def add(a,b):
+    c=a+b
+    return 
+result=add(5,4)
+print(result)"""
+def format_name(f_name,l_name):
+    formatted_f_name=f_name.title()
+    formatted_l_name=l_name.title()
+    return f"{formatted_f_name,formatted_l_name}"
+
+print(format_name("RAM","KAustubh"))
+
+
+
+
+
+
+
+
+
+
+
+
+
